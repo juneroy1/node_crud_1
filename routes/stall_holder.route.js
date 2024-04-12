75e50de346ef666f14d5cb6a0ea5964e717b80cc
Juneroy Quinimon <eroyjune@gmail.com>
2024-04-12 21:05:55 +0800

    feat: created new routes for creating new stallholder

--- routes/stall_holder.route.js
+++ routes/stall_holder.route.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { getStallHolders, getStallHolderById } = require('../controllers/stall_holder.controller');
+const { getStallHolders, getStallHolderById, createStallHolder } = require('../controllers/stall_holder.controller');
 const router = express.Router()
 
 
 router.get("/", getStallHolders);
 router.get("/:id", getStallHolderById);
+router.post("/", createStallHolder);
 
 
 module.exports = router

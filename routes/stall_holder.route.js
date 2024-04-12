c41100a4dd89ba8cf35da666bdfd6846c1f48870
Juneroy Quinimon <eroyjune@gmail.com>
2024-04-12 21:28:28 +0800

    feat: create new route for updating the stall holder by id

--- routes/stall_holder.route.js
+++ routes/stall_holder.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const { getStallHolders, getStallHolderById, createStallHolder } = require('../controllers/stall_holder.controller');
+const { getStallHolders, getStallHolderById, createStallHolder, updateStallHolderById } = require('../controllers/stall_holder.controller');
 const router = express.Router()
 
 
 router.get("/", getStallHolders);
 router.get("/:id", getStallHolderById);
 router.post("/", createStallHolder);
+router.put("/:id", updateStallHolderById);
 
 
 module.exports = router

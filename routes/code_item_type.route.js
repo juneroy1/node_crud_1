74eba92d26818392a4e93a591720ff1b18b4d57b
Juneroy Quinimon <eroyjune@gmail.com>
2024-04-20 01:31:17 +0800

    feat: create new route for creating code item type

--- routes/code_item_type.route.js
+++ routes/code_item_type.route.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
   getCodeItemTypes,
   getCodeItemTypeById,
+  createCodeItemType,
 } = require("../controllers/code_item_type.controller");
 
 const router = express.Router();
 
 router.get("/", getCodeItemTypes);
 router.get("/:id", getCodeItemTypeById);
+router.post("/", createCodeItemType);
 
 module.exports = router;

16f1ca89e2b56d4055273265b247072d2dad3d1a
Juneroy Quinimon <eroyjune@gmail.com>
2024-04-20 01:48:40 +0800

    feat: created new route for deleting and updating code item type

--- routes/code_item_type.route.js
+++ routes/code_item_type.route.js
@@ -3,6 +3,8 @@ const {
   getCodeItemTypes,
   getCodeItemTypeById,
   createCodeItemType,
+  updateCodeItemType,
+  deleteCodeItemType,
 } = require("../controllers/code_item_type.controller");
 
 const router = express.Router();
@@ -10,5 +12,7 @@ const router = express.Router();
 router.get("/", getCodeItemTypes);
 router.get("/:id", getCodeItemTypeById);
 router.post("/", createCodeItemType);
+router.put("/:id", updateCodeItemType);
+router.delete("/:id", deleteCodeItemType);
 
 module.exports = router;
